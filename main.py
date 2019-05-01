9ef32efd69e497fccc8c9f277b7a40d02a2d67a6
Brian Nisonger <vanulor@gmail.com>
2019-04-30 20:41:03 -0700

    Code working

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
+from re import sub
 
 import requests
 from flask import Flask, send_file, Response
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-
 
 def get_fact():
 
@@ -19,10 +19,13 @@ def get_fact():
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    payload={"input_text":sub('[^A-Za-z0-9 ]+', '', get_fact())} #regex cleans out anything that isn't alphanum or space
+    r = requests.post("https://hidden-journey-62459.herokuapp.com/piglatinize/", data=payload)
+    return f"<HTML><body><a href='{r.url}'>{r.url}</a><p>{r.text}</body></HTML>"
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
-    app.run(host='0.0.0.0', port=port)
+    host=os.environ.get("HOST","127.0.0.1")
+    app.run(host=host, port=port)
 
