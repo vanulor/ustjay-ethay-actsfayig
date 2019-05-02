8944a58fe75b3adefd372c73ce9c0e2b44418448
Brian Nisonger <vanulor@gmail.com>
2019-05-02 01:27:51 -0700

    Fixed hostname

--- main.py
+++ main.py
@@ -26,6 +26,5 @@ def home():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
-    host=os.environ.get("HOSTNAME","127.0.0.1")
-    app.run(host=host, port=port)
+    app.run('0.0.0.0', port=port)
 
