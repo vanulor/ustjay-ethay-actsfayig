c6fde19916ab1a7ef184b635ef2e9ff20a232d93
Brian Nisonger <vanulor@gmail.com>
2019-05-02 01:35:29 -0700

    Ran code through YAPF..final commit

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+
 def get_fact():
 
     response = requests.get("http://unkno.com")
@@ -19,12 +20,15 @@ def get_fact():
 
 @app.route('/')
 def home():
-    payload={"input_text":sub('[^A-Za-z0-9 ]+', '', get_fact())} #regex cleans out anything that isn't alphanum or space
-    r = requests.post("https://hidden-journey-62459.herokuapp.com/piglatinize/", data=payload)
+    payload = {
+        "input_text": sub('[^A-Za-z0-9 ]+', '', get_fact())
+    }  #regex cleans out anything that isn't alphanum or space
+    r = requests.post(
+        "https://hidden-journey-62459.herokuapp.com/piglatinize/",
+        data=payload)
     return f"<HTML><body><a href='{r.url}'>{r.url}</a><p>{r.text}</body></HTML>"
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run('0.0.0.0', port=port)
-
